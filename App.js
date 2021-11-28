45a7b89b7b2bbf5ea64888291fafaeaf33c9c8f0
Rajkumar Rajan <rajredwingz@gmail.com>
2021-11-29 00:50:53 +0530

    Save

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from "./components/Header";
